b984796ac4b2cd3b3960b64c96108efdf3de12f5
Rodion Kosyanenko <GoodWasHere@gmail.com>
2019-12-30 11:13:48 +0600

    Set version to 0.7.dev1+secp256k1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name = 'jwcrypto',
-    version = '0.7.dev1',
+    version = '0.7.dev1+secp256k1',
     license = 'LGPLv3+',
     maintainer = 'JWCrypto Project Contributors',
     maintainer_email = 'simo@redhat.com',
